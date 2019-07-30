2e6c2d4e61e47cc14975743007e2375634938ad9
vinaysharma14 <vinay@geekyants.com>
2019-07-30 13:24:16 +0530

    Updates

---
+++ precache-manifest.4b0da9f6c58b9d02d586b3e83b50f400.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab8f2e4353bf9d8bc940f4fa6cd7031",
+    "url": "/step-sequencer/index.html"
+  },
+  {
+    "revision": "d9e771237b11d734debe",
+    "url": "/step-sequencer/static/css/main.e0d2dd0c.chunk.css"
+  },
+  {
+    "revision": "540444ad9b5462670050",
+    "url": "/step-sequencer/static/js/2.07866aaf.chunk.js"
+  },
+  {
+    "revision": "d9e771237b11d734debe",
+    "url": "/step-sequencer/static/js/main.8d24723d.chunk.js"
+  },
+  {
+    "revision": "df6396077645acbc05b1",
+    "url": "/step-sequencer/static/js/runtime~main.f37c02fa.js"
+  },
+  {
+    "revision": "c443e2969a94d4d9d99148ffff708677",
+    "url": "/step-sequencer/static/media/808.c443e296.wav"
+  },
+  {
+    "revision": "b64371abf820aeec78ded4d013793010",
+    "url": "/step-sequencer/static/media/clap.b64371ab.wav"
+  },
+  {
+    "revision": "1b2e37458f391eaa6bc4031c1cd02563",
+    "url": "/step-sequencer/static/media/kick.1b2e3745.wav"
+  },
+  {
+    "revision": "e5e3392ea867374473b6a28311f396d5",
+    "url": "/step-sequencer/static/media/metronome.e5e3392e.png"
+  },
+  {
+    "revision": "9ef541f11ae94d2e2d2129769a76f782",
+    "url": "/step-sequencer/static/media/ride.9ef541f1.wav"
+  },
+  {
+    "revision": "f8fee813d5fbc32b46dadbf2c6be6753",
+    "url": "/step-sequencer/static/media/snare.f8fee813.wav"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/step-sequencer/static/media/tick.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/step-sequencer/static/media/tock.fe0ded65.wav"
+  },
+  {
+    "revision": "24821dd029081fb9467825c57b6705bc",
+    "url": "/step-sequencer/static/media/trap.24821dd0.wav"
+  }
+]);
